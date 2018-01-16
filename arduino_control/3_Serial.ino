9a37552396efd70206b45d018e6631c83b2dd782
Daniel Kummer <daniel.kummer@gmail.com>
2018-01-16 23:22:49 +0100

    Update arduino serial interface

--- arduino_control/3_Serial.ino
+++ arduino_control/3_Serial.ino
@@ -24,8 +24,7 @@ void handleCommand() {
             
     Serial.println(F("info   get info"));
     return;
-  } else  
-  if(startsWith("info",received_command_data_buffer)) {    
+  } else if(startsWith("info",received_command_data_buffer)) {    
     log("buffer=%s", nixieControl.nixie_buffer);    
     log("hv=%d", nixieControl.hv_power);
     log("interval=%d", nixieControl.Interval);    
@@ -39,10 +38,46 @@ void handleCommand() {
     log("pixelDirection=%d", matrix.Direction);
     log("pixelBrightness=%d", matrix.getBrightness());
     log("buttonColor1=%d,%d,%d", buttonLed.Red(buttonLed.Color1), buttonLed.Green(buttonLed.Color1), buttonLed.Blue(buttonLed.Color1) );    
-  } else  
-  if(startsWith("pixel",received_command_data_buffer)) {
-    char* matrixCommand = &received_command_data_buffer[6];     
-    if(startsWith("V",matrixCommand)) {    
+  } else if(startsWith("pixel",received_command_data_buffer)) {
+    char* matrixCommand = &received_command_data_buffer[5];     
+    handlePixelCommand(matrixCommand);    
+  } else if(startsWith("nixie",received_command_data_buffer)) {      
+    char* nixieCommand = &received_command_data_buffer[5]; 
+    handleNixieCommand(nixieCommand);
+  } else if(startsWith("buttonColor1=",received_command_data_buffer)) {        
+      uint32_t color = getIntColor(&received_command_data_buffer[13]);    
+      buttonLed.ColorSet(color);
+      buttonLed.Color1 = color;       
+  } else {
+    Serial.println(F("error=unknown command"));
+    return;
+  }
+  Serial.println(F("success"));      
+}
+
+uint32_t getIntColor(const char* intcolors) {
+  char delimiter[] = ",";
+  char *ptr;
+
+  // initialisieren und ersten Abschnitt erstellen
+  ptr = strtok(intcolors, delimiter); 
+  return matrix.Color(atoi(ptr), atoi(strtok(NULL, delimiter)), atoi(strtok(NULL, delimiter)));
+}
+
+bool startsWith(const char *pre, const char *str) {
+  return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+bool isOn(char* str) {
+  return startsWith("=ON", str);
+}
+
+bool isOff(char* str) {
+  return startsWith("=OFF", str);
+}
+
+void handlePixelCommand(const char* matrixCommand) {
+  if(startsWith("V",matrixCommand)) {    
       matrix.ColorSet(isOn(&matrixCommand[1]) ? matrix.Color1 : 0);
       matrix.setBrightness(isOn(&matrixCommand[1]) ? 255 : 0);           
     } else     
@@ -107,80 +142,40 @@ void handleCommand() {
       uint32_t color = getIntColor(&matrixCommand[7]);        
       matrix.Color2 = color;   
     }      
-  } else 
-  // end matrix*
-  if(startsWith("nixie",received_command_data_buffer)) {      
-    char* nixieCommand = &received_command_data_buffer[6]; 
-    if(startsWith("V",nixieCommand)) {
-      nixieControl.hv_power = isOn(&nixieCommand[1]) ? 1 : 0;  
-    } else      
-    if(startsWith("Interval=",nixieCommand)) {    
-      int interval = atoi(&nixieCommand[9]);
-      if(interval > 0) {
-        nixieControl.Interval = interval;      
-      }    
-    } else   
-    if(startsWith("Write=",nixieCommand)) {    
-      char* bufferData = &nixieCommand[6];
-      strncpy(nixieControl.nixie_buffer, bufferData, 9);    
-    } else
-    if(startsWith("Run=",nixieCommand)) {    
-      char* mode = &nixieCommand[4];
-      
-      if(startsWith("CO",mode)) {    //COUNTER    
-        nixieControl.SimpleCounter(100);    
-      } else
-      if(startsWith("T_CO",mode)) { //T_COUNTER
-        nixieControl.TrippleCounter(100);    
-      } else
-      if(startsWith("DI",mode)) { //DISPLAY
-        nixieControl.Display(50);          
-      } else
-      if(startsWith("SC",mode)) { //SCROLL
-        nixieControl.Scroll(100);    
-      } else 
-      if(startsWith("PU",mode)) { //PULSE
-        nixieControl.PulseOnUpdate(100);                
-        //SMNixie.Set(FlashOnUpdateMode);                   
-        //matrix.ActivePattern = NONE;
-      } else   
-      if(startsWith("WH",mode)) { //WHIPE
-        nixieControl.Whipe();                      
-      } else {
-        Serial.println(F("error=unknown mode"));       
-      }  
-    } else
-    // end nixie*    
-    if(startsWith("buttonColor1=",received_command_data_buffer)) {        
-      uint32_t color = getIntColor(&received_command_data_buffer[13]);    
-      buttonLed.ColorSet(color);
-      buttonLed.Color1 = color;       
+}
+
+void handleNixieCommand(const char* nixieCommand) {
+  if(startsWith("V",nixieCommand)) {
+    nixieControl.hv_power = isOn(&nixieCommand[1]) ? 1 : 0;  
+  } else if(startsWith("Interval=",nixieCommand)) {    
+    int interval = atoi(&nixieCommand[9]);
+    if(interval > 0) {
+      nixieControl.Interval = interval;      
+    }    
+  } else if(startsWith("Write=",nixieCommand)) {    
+    char* bufferData = &nixieCommand[6];
+    strncpy(nixieControl.nixie_buffer, bufferData, 9);    
+  } else if(startsWith("Run=",nixieCommand)) {    
+    char* mode = &nixieCommand[4];
+    
+    if(startsWith("CO",mode)) {    //COUNTER    
+      nixieControl.SimpleCounter(100);    
+    } else if(startsWith("T_CO",mode)) { //T_COUNTER
+      nixieControl.TrippleCounter(100);    
+    } else if(startsWith("DI",mode)) { //DISPLAY
+      nixieControl.Display(50);          
+    } else if(startsWith("SC",mode)) { //SCROLL
+      nixieControl.Scroll(100);    
+    } else if(startsWith("PU",mode)) { //PULSE
+      nixieControl.PulseOnUpdate(100);                
+      //SMNixie.Set(FlashOnUpdateMode);                   
+      //matrix.ActivePattern = NONE;
+    } else if(startsWith("WH",mode)) { //WHIPE
+      nixieControl.Whipe();                      
     } else {
-      Serial.println(F("error=unknown command"));
-      return;
-    }
-    Serial.println(F("success"));    
+      Serial.println(F("error=unknown mode"));       
+    }  
   }
-}
-
-uint32_t getIntColor(const char* intcolors) {
-  char delimiter[] = ",";
-  char *ptr;
-
-  // initialisieren und ersten Abschnitt erstellen
-  ptr = strtok(intcolors, delimiter); 
-  return matrix.Color(atoi(ptr), atoi(strtok(NULL, delimiter)), atoi(strtok(NULL, delimiter)));
-}
-
-bool startsWith(const char *pre, const char *str) {
-  return strncmp(pre, str, strlen(pre)) == 0;
-}
-
-bool isOn(char* str) {
-  return startsWith("=ON", str);
-}
-
-bool isOff(char* str) {
-  return startsWith("=OFF", str);
-}
+}  
+  
 
